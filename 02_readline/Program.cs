f3bec7b2541192e4a5c8bc1b6d2ea31bdfb878f8
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-09-24 09:10:59 +0300

    Визначення ціни подорожі

---
+++ 02_readline/Program.cs
@@ -0,0 +1,12 @@
+﻿// Завдання: 
+// Користувач вводить розхід авто (л/100км) та відстань. Визначити ціну поїздки.
+Console.WriteLine("Enter your car consumption (l/100km): ");
+float consumption = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your trip distance (km): ");
+float distance = float.Parse(Console.ReadLine());
+
+const decimal fuelPrice = 55;
+
+decimal price = (decimal)((distance / 100 * consumption) * (float)fuelPrice);
+Console.WriteLine($"Trip price: {price}uah");
