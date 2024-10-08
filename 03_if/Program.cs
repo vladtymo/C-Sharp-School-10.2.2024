edf528dfcd667256c0c3830228dc2859b638f541
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-10-08 09:37:39 +0300

    Create game with if else

---
+++ 03_if/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// > < >= <= == !=
+// if switch
+var rand = new Random();
+
+int score = 0;
+int count = 0;
+
+while (true)
+{
+    Console.Clear();
+    
+    Console.Write("Enter your rate (1-6): ");
+    int rate = Convert.ToInt32(Console.ReadLine());
+    
+    int number = rand.Next(1, 7); // генерація випадкового числа з 1 до 7 (не включно)
+    Console.WriteLine($"The number is {number}");
+
+    if (rate == number)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("You won! Thanks for playing!");
+        score += 10;
+        count++;
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("You loose!");
+        score -= 5;
+        count = 0;
+    }
+
+    if (count == 2)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Good combination!");
+        score += 50;
+    }
+
+    if (count == 3)
+    {
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine("Jackpot!");
+        score += 100;
+    }
+
+    Console.ResetColor();
+
+    Console.WriteLine($"Score: {score}");
+    
+    Console.WriteLine("Press any key to continue...");
+    Console.ReadKey();
+}
+
+
+
+//... end!
