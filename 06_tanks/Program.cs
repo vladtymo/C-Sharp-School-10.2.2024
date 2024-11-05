d394cdc88cbc691bfe7ae50bbbdb0010233c4f13
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-11-05 09:34:04 +0200

    Start tank gaem

---
+++ 06_tanks/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        DrawTankUp();
+        Console.WriteLine();
+        DrawTankDown();
+    }
+    
+    static void DrawTankUp()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("  █  ");
+        Console.WriteLine("█████");
+        Console.WriteLine("██ ██");
+        Console.ResetColor();
+    }
+    static void DrawTankDown()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("██ ██");
+        Console.WriteLine("█████");
+        Console.WriteLine("  █  ");
+        Console.ResetColor();
+    }
+}
