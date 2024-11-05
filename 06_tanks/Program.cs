476c9680c30d4b79a22c4e60b99afa249dfdc73b
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-11-05 09:37:49 +0200

    Update

--- 06_tanks/Program.cs
+++ 06_tanks/Program.cs
@@ -5,22 +5,42 @@
         DrawTankUp();
         Console.WriteLine();
         DrawTankDown();
+        Console.WriteLine();
+        DrawLeft();
+        Console.WriteLine();
+        DrawRight();
     }
     
     static void DrawTankUp()
     {
         Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("  █  ");
-        Console.WriteLine("█████");
-        Console.WriteLine("██ ██");
+        Console.WriteLine("  ██  ");
+        Console.WriteLine("██████");
+        Console.WriteLine("██  ██");
         Console.ResetColor();
     }
     static void DrawTankDown()
     {
         Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("██ ██");
-        Console.WriteLine("█████");
-        Console.WriteLine("  █  ");
+        Console.WriteLine("██  ██");
+        Console.WriteLine("██████");
+        Console.WriteLine("  ██  ");
+        Console.ResetColor();
+    }
+    static void DrawLeft()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("  ████");
+        Console.WriteLine("████  ");
+        Console.WriteLine("  ████");
+        Console.ResetColor();
+    }
+    static void DrawRight()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("████  ");
+        Console.WriteLine("  ████");
+        Console.WriteLine("████  ");
         Console.ResetColor();
     }
 }
