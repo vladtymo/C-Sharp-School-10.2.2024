37065eb318c45386169f76937be4b84e52cc86a7
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-11-12 09:03:58 +0200

    Loops

---
+++ 07_loops/Program.cs
@@ -0,0 +1,40 @@
+﻿// ---------- loops -----------
+// for(змінна; умова; вираз) {}
+
+int count = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < count; ++i)
+{
+    Console.WriteLine($"{i} - Iteration");
+    Console.WriteLine("Hello Eugen!");
+}
+
+Console.Write("Enter positive number: ");
+int number = int.Parse(Console.ReadLine());
+
+while (number <= 0)
+{
+    Console.Write("Invalid number, please enter again: ");
+    number = int.Parse(Console.ReadLine());
+}
+
+string name;
+
+do
+{
+    Console.Write("Enter your name: ");
+    name = Console.ReadLine();
+    
+} while (!char.IsUpper(name[0]));
+
+// вічний цикл
+while (true)
+{
+    Console.Write("Enter password:");
+    string pass = Console.ReadLine();
+    
+    if (pass == "admin")
+        break; // завершити цикл
+}
+
+Console.WriteLine("Have a nice day!");
