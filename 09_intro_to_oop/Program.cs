2e770c82bd33756d7f0c5545d7e44c0b181d9ff1
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-11-26 09:37:14 +0200

    Intro to OOP

---
+++ 09_intro_to_oop/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Channels;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var dog = new Dog();
+
+        dog.name = "James";
+        dog.Jump();
+        dog.SayGav();
+        
+    }
+}
+
+class Dog
+{
+    // properties
+    public double weight;
+    public string breed;
+    public string name;
+    public int age;
+    public bool isSecurityGuard;
+
+    // methods
+    public void Jump()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("I am jumping...!");
+        Console.ResetColor();
+    }
+    
+    public void SayGav()
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Gav-gav-gav!");
+        Console.ResetColor();
+    }
+    
+    public void Eat()
+    {
+        Console.WriteLine("I am eating now...!");
+        weight += 0.5;
+    }
+}
