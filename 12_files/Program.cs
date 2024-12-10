2d31c710a43e718a33113fcc67b2a4c7f77eb14c
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-12-10 09:06:39 +0200

    File example

---
+++ 12_files/Program.cs
@@ -0,0 +1,23 @@
+﻿// File path:
+// - relative path: /photos/image.png
+// - absolute path C://Users/vlad/photos/image.png
+
+ var fs = File.CreateText(@"test.txt");
+
+ fs.WriteLine("Hello my dear file!");
+ fs.WriteLine("How are you doing?");
+ fs.WriteLine($"My name is {Console.ReadLine()}!!!");
+
+ fs.Close();
+
+File.Copy(@"test.txt", Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/copy_file.txt");
+
+// works on Windows only
+// File.Encrypt("test.txt");
+
+var reader = File.OpenText("test.txt");
+
+Console.WriteLine(reader.ReadLine());
+Console.WriteLine(reader.ReadToEnd());
+
+File.Delete("test.txt");
