8e051ef7949be04e78c12ee2654ad8f9584305e1
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2025-01-21 09:08:18 +0200

    Stack example

---
+++ 15_stack/Program.cs
@@ -0,0 +1,23 @@
+﻿Stack<string> stack = new Stack<string>();
+
+stack.Push("Volodia");
+stack.Push("Lera");
+stack.Push("Andrii");
+stack.Push("Eugen");
+stack.Push("Maks");
+
+Console.WriteLine("Top item: " + stack.Peek()); // показати верхній елемент
+
+Console.WriteLine("Top item: " + stack.Pop()); // повернути та видалити верхній елемент
+
+Console.WriteLine("Students" + stack.Count); // 4
+
+Console.WriteLine("Maks: " + stack.Contains("Maks"));
+
+Console.WriteLine("Students:");
+// поки елементи є
+while (stack.Count > 0)
+{
+    Console.WriteLine(stack.Pop());
+}
+
