7132b9b8e0f7d31e6081ab3159774ba9274a774a
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2025-01-28 09:12:58 +0200

    Stack example

--- 15_stack/Program.cs
+++ 15_stack/Program.cs
@@ -21,3 +21,42 @@ while (stack.Count > 0)
     Console.WriteLine(stack.Pop());
 }
 
+string goodExp = "2 + (1 * [1:1] + x) { ( 44 + 4 [9-9] ) }";
+string badExp = "2 + (1 * {1:1] + x)) { [ 44 + 4 [9-9] ) })";
+
+void CheckExp(string exp)
+{
+    Dictionary<char, char> dict = new Dictionary<char, char>()
+    {
+        // ключ = значення
+        [')'] = '(',
+        [']'] = '[',
+        ['}'] = '{'
+    };
+    
+    Stack<char> brackets = new();
+
+    foreach (var s in exp)
+    {
+        if (dict.ContainsValue(s))
+            brackets.Push(s);
+
+        if (dict.ContainsKey(s))
+        {
+            if (brackets.Pop() != dict[s])
+            {
+                Console.WriteLine("Expression is invalid!");
+                return;
+            }
+        }
+    }
+
+    if (brackets.Count == 0)
+        Console.WriteLine("Expression is correct");
+    else
+        Console.WriteLine("Expression is incorrect");
+}
+
+CheckExp("(2 + [1+1])");
+CheckExp(goodExp);
+CheckExp(badExp);
